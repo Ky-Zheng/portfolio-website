41f080b1a3bacc0c90abb753823297fdd04ec245
ky-zheng <gokaiyuezheng@gmail.com>
2024-02-01 20:42:14 -0500

    email test passed

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -6,25 +6,24 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = await req.json();
-  const { email, subject, message } = body;
+  const { email, subject, message } = await req.json();
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["gokaiyuezheng@gmail.com", email],
-      subject: "Hello world",
+      to: [fromEmail, email],
+      subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New </p>
+          <p>Thank you for contacting me!</p>
+          <p>New message sumbmited:</p>
           <p>{message}</p>
         </>
       ),
     });
 
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    return NextResponse.json({ error });
   }
 }
--- src/app/componets/EmailSection.jsx
+++ src/app/componets/EmailSection.jsx
@@ -17,7 +17,8 @@ const EmailSection = () => {
       message: e.target.message.value,
     };
     const JSONdata = JSON.stringify(data);
-    const endpoint = "api/send";
+    const endpoint = "/api/send";
+
     const options = {
       method: "POST",
       headers: {
@@ -25,11 +26,13 @@ const EmailSection = () => {
       },
       body: JSONdata,
     };
+
     const response = await fetch(endpoint, options);
     const resData = await response.json();
+    console.log(resData);
 
-    if (resData.status === 200) {
-      console.log("Message sent");
+    if (response.status === 200) {
+      console.log("Message sent.");
       setEmailSubmitted(true);
     }
   };
@@ -57,66 +60,66 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col">
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className=" text-white block mb-2 text-sm font-medium"
+        {emailSubmitted ? (
+          <p className="text-green-500 text-sm mt-2">
+            Email sent successfully!
+          </p>
+        ) : (
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className=" text-white block mb-2 text-sm font-medium"
+              >
+                Your Email
+              </label>
+              <input
+                name="email"
+                type="email"
+                id="email"
+                required
+                className="bg-[#18191E] border border-[#3f333f] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="amazing.human@google.com"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="subject"
+                className=" text-white block mb-2 text-sm font-medium"
+              >
+                Subject
+              </label>
+              <input
+                name="subject"
+                type="text"
+                id="subject"
+                required
+                className="bg-[#18191E] border border-[#3f333f] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Just Saying Hi"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className=" text-white block mb-2 text-sm font-medium"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="bg-[#18191E] border border-[#3f333f] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Hi Kaiyue, I'd like to connect with you!"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-2.5 rounded-lg w-full "
             >
-              Your Email
-            </label>
-            <input
-              name="email"
-              type="email"
-              id="email"
-              required
-              className="bg-[#18191E] border border-[#3f333f] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="amazing.human@google.com"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="subject"
-              className=" text-white block mb-2 text-sm font-medium"
-            >
-              Subject
-            </label>
-            <input
-              name="subject"
-              type="text"
-              id="subject"
-              required
-              className="bg-[#18191E] border border-[#3f333f] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Just Saying Hi"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className=" text-white block mb-2 text-sm font-medium"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              required
-              className="bg-[#18191E] border border-[#3f333f] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Hi Kaiyue, I'd like to connect with you!"
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-2.5 rounded-lg w-full "
-          >
-            Send Message
-          </button>
-          {emailSubmitted && (
-            <p className="text-green-500 text-sm mt-2">
-              Email Sent Successfully!
-            </p>
-          )}
-        </form>
+              Send Message
+            </button>
+          </form>
+        )}
       </div>
     </section>
   );
