b3e9db927abc5ed99770bb3ffcb41d5a8d4e68e9
ky-zheng <gokaiyuezheng@gmail.com>
2024-02-02 09:26:59 -0500

    reverse number animation

--- src/app/componets/AchievementsSection.jsx
+++ src/app/componets/AchievementsSection.jsx
@@ -19,16 +19,16 @@ const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl-px-16">
       <div className="border-[#33353F] border rouded-md py-8 px-17 flex flex-row items-center justify-between">
-        {achievementsList.map((achievementsList, index) => {
+        {achievementsList.map((achievement, index) => {
           return (
             <div
               key={index}
               className="flex flex-col items-center justify-center mx-4 "
             >
               <h2 className="text-white text-4xl font-bold">
-                {achievementsList.value}
+                {achievement.value}
               </h2>
-              <p className="text-[#ADB7BE]">{achievementsList.metric}</p>
+              <p className="text-[#ADB7BE]">{achievement.metric}</p>
             </div>
           );
         })}
