7e64ffbab6e18aba20ddadc185559e82b8d602a9
ky-zheng <gokaiyuezheng@gmail.com>
2024-02-04 00:51:15 -0500

    add link

--- src/app/componets/HeroSection.jsx
+++ src/app/componets/HeroSection.jsx
@@ -38,14 +38,20 @@ const HeroSection = () => {
             day.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-tertiary-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white">
-              <Link href="#contact">Hire Me</Link>
-            </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-tertiary-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
+            <Link
+              href="#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="#contact"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                <Link href="#contact">Resume</Link>
+                Resume
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div className="col-span-4 place-self-center mt-4 lg:mt-0">
